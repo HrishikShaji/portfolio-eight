67f735c29f5fcc57b8330bf735aae88716d3cae9
HrishikShaji <hrishikminishaji@gmail.com>
2024-02-15 23:42:00 +0530

    animation works but all on same line

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -4,42 +4,60 @@ import { data } from "../lib/data";
 import { useRef } from "react";
 import gsap from "gsap";
 import SplitType from "split-type";
+import { LinePath } from "./LinePath";
 
 export const About = () => {
-	const paraRef = useRef<HTMLParagraphElement>(null);
-	const targetRef = useRef<HTMLDivElement>(null);
+  const paraRef = useRef<HTMLParagraphElement>(null);
+  const lineRef = useRef<HTMLDivElement>(null);
+  useGSAP(() => {
+    if (paraRef.current) {
+      const text = new SplitType(paraRef.current, { types: "lines" });
+      const linePath = document.querySelector(".line-path");
 
-	useGSAP(() => {
-		if (paraRef.current) {
-			const text = new SplitType(paraRef.current, { types: "lines" });
-			const linePath = document.querySelector(".line-path");
-			text.lines?.forEach((line) => {
-				const tl = gsap.timeline();
+      // Calculate the total duration of animation
+      const totalDuration = 2; // Adjust this value as needed
 
-				gsap.to(line, {
-					motionPath: {
-						path: linePath,
-					},
-					scrollTrigger: {
-						trigger: line,
-						scrub: true,
-						start: "top 90%",
-						end: "top center",
-						markers: true,
-					},
-				});
-			});
-		}
-	}, {});
+      // Loop through each line and create animations
+      text.lines?.forEach((line, index) => {
+        const tl = gsap.timeline({
+          delay: index * (totalDuration / text.lines.length), // Delay each animation based on index
+          onComplete: () => {
+            if (index === text.lines.length - 1) {
+              // Animation complete
+            }
+          },
+        });
 
-	return (
-		<div className="  text-black py-40 w-full relative flex justify-center  items-center ">
-			<h3
-				ref={paraRef}
-				className="text-3xl absolute left-[calc(15%_+_250px)] w-[50%] leading-[50px] text-neutral-300"
-			>
-				{data.about.description}
-			</h3>
-		</div>
-	);
+        tl.to(line, {
+          motionPath: {
+            path: linePath,
+            align: linePath,
+            start: 1,
+            end: 0,
+          },
+          scrollTrigger: {
+            trigger: line,
+            scrub: true,
+            start: "top 90%",
+            end: "top 10%",
+            markers: true,
+          },
+        });
+      });
+    }
+  }, []);
+
+  return (
+    <div className="  text-black py-40 w-full relative flex justify-center  items-center ">
+      <div ref={lineRef} className="h-screen absolute left-[40%] w-[15%] z-20">
+        <LinePath />
+      </div>
+      <h3
+        ref={paraRef}
+        className="text-3xl absolute left-[calc(15%_+_250px)] w-[50%] leading-[50px] text-neutral-300"
+      >
+        {data.about.description}
+      </h3>
+    </div>
+  );
 };
--- app/components/Sample.tsx
+++ app/components/Sample.tsx
@@ -7,129 +7,114 @@ import { About } from "./About";
 import { LinePath } from "./LinePath";
 
 export const Sample = () => {
-	const circleRef = useRef<HTMLDivElement>(null);
-	const mainRef = useRef<HTMLDivElement>(null);
-	const lineRef = useRef<HTMLDivElement>(null);
+  const circleRef = useRef<HTMLDivElement>(null);
+  const mainRef = useRef<HTMLDivElement>(null);
+  const lineRef = useRef<HTMLDivElement>(null);
 
-	useGSAP(() => {
-		if (mainRef.current && circleRef.current && lineRef.current) {
-			gsap.to(circleRef.current, {
-				scrollTrigger: {
-					trigger: mainRef.current,
-					start: "top top",
-					end: "bottom bottom",
-					pin: circleRef.current,
-					scrub: true,
-				},
-			});
-			gsap.to(lineRef.current, {
-				scrollTrigger: {
-					trigger: mainRef.current,
-					start: "top top",
-					end: "bottom bottom",
-					pin: lineRef.current,
-					scrub: true,
-				},
-			});
-		}
-	}, {});
+  useGSAP(() => {
+    if (mainRef.current && circleRef.current) {
+      gsap.to(circleRef.current, {
+        scrollTrigger: {
+          trigger: mainRef.current,
+          start: "top top",
+          end: "bottom bottom",
+          pin: circleRef.current,
+          scrub: true,
+        },
+      });
+    }
+  }, {});
 
-	return (
-		<div
-			ref={mainRef}
-			className="w-full bg-neutral-400 min-h-screen items-center relative flex flex-col"
-		>
-			<div className="h-screen relative w-full flex  items-center">
-				<div
-					ref={lineRef}
-					className="h-screen absolute left-[40%] w-[15%] z-20"
-				>
-					<LinePath />
-				</div>
-				<div
-					ref={circleRef}
-					className="h-[500px] absolute left-[15%] w-[500px] rounded-full bg-teal-500 "
-				></div>
-			</div>
-			<About />
-			<div className="h-full w-full flex flex-col gap-20 items-center">
-				{Array.from({ length: 10 }).map((_, i) => (
-					<Circle key={i} />
-				))}
-			</div>
-			<div className="h-full w-full flex flex-col gap-40 items-center">
-				{Array.from({ length: 5 }).map((_, i) => (
-					<Square key={i} />
-				))}
-			</div>
-			<div className="h-full w-full flex flex-col gap-40 items-center">
-				{Array.from({ length: 5 }).map((_, i) => (
-					<Rectangle key={i} />
-				))}
-			</div>
-			<Spacer />
-			<Spacer />
-		</div>
-	);
+  return (
+    <div
+      ref={mainRef}
+      className="w-full bg-neutral-400 min-h-screen items-center relative flex flex-col"
+    >
+      <div className="h-screen relative w-full flex  items-center">
+        <div
+          ref={circleRef}
+          className="h-[500px] absolute left-[15%] w-[500px] rounded-full bg-teal-500 "
+        ></div>
+      </div>
+      <About />
+      <div className="h-full w-full flex flex-col gap-20 items-center">
+        {Array.from({ length: 10 }).map((_, i) => (
+          <Circle key={i} />
+        ))}
+      </div>
+      <div className="h-full w-full flex flex-col gap-40 items-center">
+        {Array.from({ length: 5 }).map((_, i) => (
+          <Square key={i} />
+        ))}
+      </div>
+      <div className="h-full w-full flex flex-col gap-40 items-center">
+        {Array.from({ length: 5 }).map((_, i) => (
+          <Rectangle key={i} />
+        ))}
+      </div>
+      <Spacer />
+      <Spacer />
+    </div>
+  );
 };
 
 const Rectangle = () => {
-	const rectangleRef = useRef<HTMLDivElement>(null);
+  const rectangleRef = useRef<HTMLDivElement>(null);
 
-	useGSAP(() => {
-		if (rectangleRef.current) {
-			gsap.to(rectangleRef.current, {
-				x: 300,
-				scrollTrigger: {
-					trigger: rectangleRef.current,
-					start: "top 80%",
-					end: "top center",
-					scrub: true,
-				},
-			});
-		}
-	}, {});
+  useGSAP(() => {
+    if (rectangleRef.current) {
+      gsap.to(rectangleRef.current, {
+        x: 300,
+        scrollTrigger: {
+          trigger: rectangleRef.current,
+          start: "top 80%",
+          end: "top center",
+          scrub: true,
+        },
+      });
+    }
+  }, {});
 
-	return (
-		<div ref={rectangleRef} className="h-40 w-80 bg-pink-500 rounded-md" />
-	);
+  return (
+    <div ref={rectangleRef} className="h-40 w-80 bg-pink-500 rounded-md" />
+  );
 };
 
 const Square = () => {
-	const squareRef = useRef<HTMLDivElement>(null);
+  const squareRef = useRef<HTMLDivElement>(null);
 
-	useGSAP(() => {
-		if (squareRef.current) {
-			gsap.to(squareRef.current, {
-				x: 300,
-				scrollTrigger: {
-					trigger: squareRef.current,
-					start: "top 80%",
-					end: "top center",
-					scrub: true,
-				},
-			});
-		}
-	}, {});
+  useGSAP(() => {
+    if (squareRef.current) {
+      gsap.to(squareRef.current, {
+        x: 300,
+        scrollTrigger: {
+          trigger: squareRef.current,
+          start: "top 80%",
+          end: "top center",
+          scrub: true,
+        },
+      });
+    }
+  }, {});
 
-	return <div ref={squareRef} className="h-40 w-40 bg-purple-500 rounded-md" />;
+  return <div ref={squareRef} className="h-40 w-40 bg-purple-500 rounded-md" />;
 };
 const Circle = () => {
-	const circleRef = useRef<HTMLDivElement>(null);
+  const circleRef = useRef<HTMLDivElement>(null);
 
-	useGSAP(() => {
-		if (circleRef.current) {
-			gsap.to(circleRef.current, {
-				x: 300,
-				scrollTrigger: {
-					trigger: circleRef.current,
-					start: "top 80%",
-					end: "top center",
-					scrub: true,
-				},
-			});
-		}
-	}, {});
+  useGSAP(() => {
+    if (circleRef.current) {
+      gsap.to(circleRef.current, {
+        x: 300,
+        scrollTrigger: {
+          trigger: circleRef.current,
+          start: "top 80%",
+          end: "top center",
+          scrub: true,
+        },
+      });
+    }
+  }, {});
 
-	return <div ref={circleRef} className="h-20 w-20 bg-red-500 rounded-full" />;
+  return <div ref={circleRef} className="h-20 w-20 bg-red-500 rounded-full" />;
 };
