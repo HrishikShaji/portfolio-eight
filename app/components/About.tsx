3b64060fce2b85b7b01c3dde0869bd5392296453
HrishikShaji <hrishikminishaji@gmail.com>
2024-02-15 20:05:08 +0530

    changed about animation

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -6,55 +6,48 @@ import gsap from "gsap";
 import SplitType from "split-type";
 
 export const About = () => {
-  const paraRef = useRef<HTMLParagraphElement>(null);
-  const targetRef = useRef<HTMLDivElement>(null);
+	const paraRef = useRef<HTMLParagraphElement>(null);
+	const targetRef = useRef<HTMLDivElement>(null);
 
-  useGSAP(() => {
-    if (paraRef.current && targetRef.current) {
-      const height = paraRef.current.getBoundingClientRect().height;
-      const headingHeight = targetRef.current.getBoundingClientRect().height;
-      gsap.to(targetRef.current, {
-        scrollTrigger: {
-          trigger: paraRef.current,
-          pin: targetRef.current,
-          start: "top 20%",
-          end: `+=${height - headingHeight}`,
-          scrub: true,
-        },
-      });
-    }
-    if (paraRef.current) {
-      const text = new SplitType(paraRef.current, { types: "lines" });
+	useGSAP(() => {
+		if (paraRef.current) {
+			const text = new SplitType(paraRef.current, { types: "lines" });
 
-      text.lines?.forEach((line) => {
-        gsap.to(line, {
-          x: 100,
-          scrollTrigger: {
-            trigger: line,
-            scrub: 1,
-            start: "top center",
-            end: "+=2px",
-          },
-        });
-      });
-    }
-  }, {});
+			text.lines?.forEach((line) => {
+				const tl = gsap.timeline();
 
-  return (
-    <div className="  text-black py-40  flex justify-center w-full items-center ">
-      <div className="flex w-[90vw]">
-        <div ref={targetRef} className="w-[50%] h-full ">
-          <h2 className="text-7xl text-black">ABOUT</h2>
-        </div>
-        <div className="w-[50%] h-full ">
-          <h3
-            ref={paraRef}
-            className="text-3xl leading-[50px] text-neutral-300"
-          >
-            {data.about.description}
-          </h3>
-        </div>
-      </div>
-    </div>
-  );
+				tl.to(line, {
+					x: 250,
+					scrollTrigger: {
+						trigger: line,
+						scrub: true,
+						start: "top 90%",
+						end: "top center",
+						markers: true,
+					},
+				}).to(line, {
+					x: 0,
+					immediateRender: false,
+					scrollTrigger: {
+						trigger: line,
+						scrub: true,
+						start: "top center",
+						end: "top 10%",
+						markers: true,
+					},
+				});
+			});
+		}
+	}, {});
+
+	return (
+		<div className="  text-black py-40 w-full relative flex justify-center  items-center ">
+			<h3
+				ref={paraRef}
+				className="text-3xl absolute left-[calc(15%_+_250px)] w-[50%] leading-[50px] text-neutral-300"
+			>
+				{data.about.description}
+			</h3>
+		</div>
+	);
 };
--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ import { Sample } from "./components/Sample";
 import { Spacer } from "./components/Spacer";
 import { useGSAP } from "@gsap/react";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
+import MotionPathPlugin from "gsap/dist/MotionPathPlugin";
 
-gsap.registerPlugin(useGSAP, ScrollTrigger);
+gsap.registerPlugin(useGSAP, ScrollTrigger, MotionPathPlugin);
 
 export default function Home() {
 	return (
