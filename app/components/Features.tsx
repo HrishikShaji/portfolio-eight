1da162331b3bcc11dde64395d0354f2216269a6d
HrishikShaji <hrishikminishaji@gmail.com>
2024-02-17 20:05:13 +0530

    added features

--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -9,12 +9,12 @@ export const Features = () => {
 	useGSAP(() => {
 		if (mainRef.current) {
 			gsap.from(".feature", {
-				stagger: 0.3,
+				stagger: .3,
 				y: 300,
 				scrollTrigger: {
 					trigger: mainRef.current,
-					start: "top center",
-					end: "top 40%",
+					start: "top bottom",
+					end: "top top",
 					scrub: 1,
 					markers: true,
 				},
@@ -23,11 +23,11 @@ export const Features = () => {
 	}, {});
 
 	return (
-		<div ref={mainRef} className="grid grid-cols-3 bg-yellow-500 z-40 ">
+		<div ref={mainRef} className="grid grid-cols-3  z-40 ">
 			{data.features.data.map((item, i) => (
 				<div
 					key={i}
-					className="feature gap-4 p-20  text-center w-full  flex flex-col"
+					className="feature gap-4 p-20 bg-[#fc7eff]  text-center w-full  flex flex-col"
 				>
 					<h1 className="text-4xl">{item.title}</h1>
 					<h1 className="text-xl">{item.desc}</h1>
