15aa5567c179bd09ac2c2ed6acd013195b2e5fe8
HrishikShaji <hrishikminishaji@gmail.com>
2024-02-19 23:31:11 +0530

    workheading done

--- app/components/HorizontalScroll.tsx
+++ app/components/HorizontalScroll.tsx
@@ -8,21 +8,21 @@ import { ScrollContext } from "./ScrollContext";
 import { WorkHeading } from "./WorkHeading";
 
 export const HorizontalScroll = () => {
-  const refs = useContext(ScrollContext);
-  if (!refs) return null;
+	const refs = useContext(ScrollContext);
+	if (!refs) return null;
 
-  const { targetRef, containerRef } = refs;
-  return (
-    <div
-      ref={containerRef}
-      className="  w-full flex flex-nowrap scrollbar-hide h-screen "
-    >
-      <div className="  flex" ref={targetRef}>
-        <Education />
-        <Experience />
-        <Skills />
-        <WorkHeading />
-      </div>
-    </div>
-  );
+	const { targetRef, containerRef } = refs;
+	return (
+		<div
+			ref={containerRef}
+			className="  w-full overflow-hidden flex flex-nowrap scrollbar-hide h-screen "
+		>
+			<div className="  flex" ref={targetRef}>
+				<Education />
+				<Experience />
+				<Skills />
+				<WorkHeading />
+			</div>
+		</div>
+	);
 };
--- app/components/WorkHeading.tsx
+++ app/components/WorkHeading.tsx
@@ -6,55 +6,58 @@ import { useEffect, useContext, useRef } from "react";
 import { ScrollContext } from "./ScrollContext";
 
 export const WorkHeading = () => {
-  const refs = useContext(ScrollContext);
+	const refs = useContext(ScrollContext);
 
-  const endRef = useRef<HTMLHeadingElement>(null);
-  const overlay = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    if (!refs?.scrollTween) return null;
-    if (endRef.current && overlay.current) {
-      const tl = gsap.timeline();
-      gsap.set(overlay.current, {
-        yPercent: 100,
-        display: "block",
-        transformOrigin: "bottom",
-      });
-      gsap.set(endRef.current, {
-        scale: 0,
-      });
+	const endRef = useRef<HTMLHeadingElement>(null);
+	const overlay = useRef<HTMLDivElement>(null);
+	useEffect(() => {
+		let ctx = gsap.context(() => {
+			if (endRef.current && overlay.current && refs?.scrollTween) {
+				const tl = gsap.timeline();
+				gsap.set(overlay.current, {
+					yPercent: 100,
+					display: "block",
+					transformOrigin: "bottom",
+				});
+				gsap.set(endRef.current, {
+					scale: 0,
+				});
 
-      tl.to(endRef.current, {
-        scale: 1,
-        scrollTrigger: {
-          containerAnimation: refs.scrollTween.current,
-          trigger: endRef.current,
-          start: "left 95%",
-          end: "left 50%",
-          scrub: 1,
-        },
-      });
-      gsap.to(overlay.current, {
-        yPercent: 0,
+				tl.to(endRef.current, {
+					scale: 1,
+					scrollTrigger: {
+						containerAnimation: refs.scrollTween.current,
+						trigger: endRef.current,
+						start: "left 95%",
+						end: "left 50%",
+						scrub: 1,
+					},
+				});
+				gsap.to(overlay.current, {
+					yPercent: 0,
 
-        scrollTrigger: {
-          containerAnimation: refs.scrollTween.current,
-          trigger: overlay.current,
-          start: "left 10%",
-          end: "left 1%",
-          scrub: 1,
-        },
-      });
-    }
-  }, [refs]);
-  return (
-    <div className="flex w-screen bg-[#fc7eff] overflow-hidden justify-center items-center relative">
-      <div
-        ref={overlay}
-        className="w-full z-40 h-full absolute bg-[#fc7eff] mix-blend-difference hidden top-0 left-0"
-      />
-      <h1 ref={endRef} className="text-[300px]  leading-tight">
-        MY WORK
-      </h1>
-    </div>
-  );
+					scrollTrigger: {
+						containerAnimation: refs.scrollTween.current,
+						trigger: overlay.current,
+						start: "left 10%",
+						end: "left 1%",
+						scrub: 1,
+					},
+				});
+			}
+		});
+
+		return () => ctx.kill();
+	}, [refs]);
+	return (
+		<div className="flex w-screen bg-[#fc7eff] overflow-hidden justify-center items-center relative">
+			<div
+				ref={overlay}
+				className="w-full z-40 h-full absolute bg-[#fc7eff] mix-blend-difference hidden top-0 left-0"
+			/>
+			<h1 ref={endRef} className="text-[300px]  leading-tight">
+				MY WORK
+			</h1>
+		</div>
+	);
 };
