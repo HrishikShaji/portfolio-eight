0a922a6b0c3b4753afa38ed61e70520f7ce81021
HrishikShaji <hrishikminishaji@gmail.com>
2024-02-21 18:28:34 +0530

    modified horizontal scroll

--- app/components/HorizontalScroll.tsx
+++ app/components/HorizontalScroll.tsx
@@ -12,33 +12,33 @@ import { About } from "./About";
 import { Worker } from "cluster";
 
 export const HorizontalScroll = () => {
-  const refs = useContext(ScrollContext);
-  if (!refs) return null;
+	const refs = useContext(ScrollContext);
+	if (!refs) return null;
 
-  const { targetRef, containerRef } = refs;
-  return (
-    <div
-      ref={containerRef}
-      className="  w-full overflow-hidden flex flex-nowrap scrollbar-hide h-screen "
-    >
-      <div className=" relative flex" ref={targetRef}>
-        <Education />
-        <Experience />
+	const { targetRef, containerRef } = refs;
+	return (
+		<div
+			ref={containerRef}
+			className="  w-full overflow-hidden flex flex-nowrap scrollbar-hide h-full "
+		>
+			<div className=" relative flex" ref={targetRef}>
+				<Education />
+				<Experience />
 
-        <div className=" relative  overflow-hidden">
-          <div className="absolute left-0 ">
-            <Marquee
-              speed={0.5}
-              scroll={true}
-              initialDirection={-1}
-              word="SKILLS *"
-              size="400px"
-            />
-          </div>
-        </div>
-        <Skills />
-        <WorkHeading />
-      </div>
-    </div>
-  );
+				<div className=" relative  overflow-hidden">
+					<div className="absolute left-0 ">
+						<Marquee
+							speed={0.5}
+							scroll={true}
+							initialDirection={-1}
+							word="SKILLS *"
+							size="400px"
+						/>
+					</div>
+				</div>
+				<Skills />
+				<WorkHeading />
+			</div>
+		</div>
+	);
 };
--- app/components/WorkHeading.tsx
+++ app/components/WorkHeading.tsx
@@ -6,37 +6,39 @@ import { useEffect, useContext, useRef } from "react";
 import { ScrollContext } from "./ScrollContext";
 
 export const WorkHeading = () => {
-  const refs = useContext(ScrollContext);
+	const refs = useContext(ScrollContext);
 
-  const mainRef = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    let ctx = gsap.context(() => {
-      if (mainRef.current && refs?.scrollTween) {
-        const tl = gsap.timeline();
-        tl.to(mainRef.current, {
-          scale: 0.8,
-          rotation: -5,
-          transformOrigin: "center center",
-          scrollTrigger: {
-            containerAnimation: refs.scrollTween.current,
-            trigger: mainRef.current,
-            start: "left 95%",
-            end: "left 50%",
-            scrub: 1,
-          },
-        });
-      }
-    });
+	const mainRef = useRef<HTMLDivElement>(null);
+	useEffect(() => {
+		let ctx = gsap.context(() => {
+			if (mainRef.current && refs?.scrollTween) {
+				const tl = gsap.timeline();
+				tl.to(mainRef.current, {
+					scale: 0.8,
+					rotation: -5,
+					transformOrigin: "center center",
+					scrollTrigger: {
+						containerAnimation: refs.scrollTween.current,
+						pin: mainRef.current,
+						trigger: mainRef.current,
+						start: "center 50%",
+						end: "center 50%",
+						scrub: 1,
+						markers: true,
+					},
+				});
+			}
+		});
 
-    return () => ctx.kill();
-  }, [refs]);
-  return (
-    <div
-      ref={mainRef}
-      className="flex w-screen bg-[#fc7eff] overflow-hidden justify-center items-center relative"
-    >
-      <div className="w-full z-40 h-full absolute bg-[#fc7eff] mix-blend-difference hidden top-0 left-0" />
-      <h1 className="text-[300px]  leading-tight">MY WORK</h1>
-    </div>
-  );
+		return () => ctx.kill();
+	}, [refs]);
+	return (
+		<div
+			ref={mainRef}
+			className="flex flex-col w-screen h-full bg-[#fc7eff] overflow-hidden justify-center items-center relative"
+		>
+			<div className="w-full  bg-white  h-screen " />
+			<div className="w-full  bg-red-500  h-screen " />
+		</div>
+	);
 };
