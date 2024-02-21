76a339d91e41a78dab8accd6647d3fe98657c8a7
HrishikShaji <hrishikminishaji@gmail.com>
2024-02-21 15:12:51 +0530

    added sticky animation

--- app/components/HorizontalScroll.tsx
+++ app/components/HorizontalScroll.tsx
@@ -7,35 +7,38 @@ import { Skills } from "./Skills";
 import { ScrollContext } from "./ScrollContext";
 import { WorkHeading } from "./WorkHeading";
 import Marquee from "./Marquee";
+import { Hero } from "./Hero";
+import { About } from "./About";
+import { Worker } from "cluster";
 
 export const HorizontalScroll = () => {
-	const refs = useContext(ScrollContext);
-	if (!refs) return null;
+  const refs = useContext(ScrollContext);
+  if (!refs) return null;
 
-	const { targetRef, containerRef } = refs;
-	return (
-		<div
-			ref={containerRef}
-			className="  w-full overflow-hidden flex flex-nowrap scrollbar-hide h-screen "
-		>
-			<div className="  flex" ref={targetRef}>
-				<Education />
-				<Experience />
+  const { targetRef, containerRef } = refs;
+  return (
+    <div
+      ref={containerRef}
+      className="  w-full overflow-hidden flex flex-nowrap scrollbar-hide h-screen "
+    >
+      <div className=" relative flex" ref={targetRef}>
+        <Education />
+        <Experience />
 
-				<div className=" relative  overflow-hidden">
-					<div className="absolute left-0 ">
-						<Marquee
-							speed={0.5}
-							scroll={true}
-							initialDirection={-1}
-							word="SKILLS *"
-							size="400px"
-						/>
-					</div>
-				</div>
-				<Skills />
-				<WorkHeading />
-			</div>
-		</div>
-	);
+        <div className=" relative  overflow-hidden">
+          <div className="absolute left-0 ">
+            <Marquee
+              speed={0.5}
+              scroll={true}
+              initialDirection={-1}
+              word="SKILLS *"
+              size="400px"
+            />
+          </div>
+        </div>
+        <Skills />
+        <WorkHeading />
+      </div>
+    </div>
+  );
 };
--- app/components/WorkHeading.tsx
+++ app/components/WorkHeading.tsx
@@ -6,58 +6,37 @@ import { useEffect, useContext, useRef } from "react";
 import { ScrollContext } from "./ScrollContext";
 
 export const WorkHeading = () => {
-	const refs = useContext(ScrollContext);
+  const refs = useContext(ScrollContext);
 
-	const endRef = useRef<HTMLHeadingElement>(null);
-	const overlay = useRef<HTMLDivElement>(null);
-	useEffect(() => {
-		let ctx = gsap.context(() => {
-			if (endRef.current && overlay.current && refs?.scrollTween) {
-				const tl = gsap.timeline();
-				gsap.set(overlay.current, {
-					yPercent: 100,
-					display: "block",
-					transformOrigin: "bottom",
-				});
-				gsap.set(endRef.current, {
-					scale: 0,
-				});
+  const mainRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      if (mainRef.current && refs?.scrollTween) {
+        const tl = gsap.timeline();
+        tl.to(mainRef.current, {
+          scale: 0.8,
+          rotation: -5,
+          transformOrigin: "center center",
+          scrollTrigger: {
+            containerAnimation: refs.scrollTween.current,
+            trigger: mainRef.current,
+            start: "left 95%",
+            end: "left 50%",
+            scrub: 1,
+          },
+        });
+      }
+    });
 
-				tl.to(endRef.current, {
-					scale: 1,
-					scrollTrigger: {
-						containerAnimation: refs.scrollTween.current,
-						trigger: endRef.current,
-						start: "left 95%",
-						end: "left 50%",
-						scrub: 1,
-					},
-				});
-				gsap.to(overlay.current, {
-					yPercent: 0,
-
-					scrollTrigger: {
-						containerAnimation: refs.scrollTween.current,
-						trigger: overlay.current,
-						start: "left 10%",
-						end: "left 1%",
-						scrub: 1,
-					},
-				});
-			}
-		});
-
-		return () => ctx.kill();
-	}, [refs]);
-	return (
-		<div className="flex w-screen bg-[#fc7eff] overflow-hidden justify-center items-center relative">
-			<div
-				ref={overlay}
-				className="w-full z-40 h-full absolute bg-[#fc7eff] mix-blend-difference hidden top-0 left-0"
-			/>
-			<h1 ref={endRef} className="text-[300px]  leading-tight">
-				MY WORK
-			</h1>
-		</div>
-	);
+    return () => ctx.kill();
+  }, [refs]);
+  return (
+    <div
+      ref={mainRef}
+      className="flex w-screen bg-[#fc7eff] overflow-hidden justify-center items-center relative"
+    >
+      <div className="w-full z-40 h-full absolute bg-[#fc7eff] mix-blend-difference hidden top-0 left-0" />
+      <h1 className="text-[300px]  leading-tight">MY WORK</h1>
+    </div>
+  );
 };
