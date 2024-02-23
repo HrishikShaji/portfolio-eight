c4cc2ab02f3bbe2fb3b2ec94e3888f5085499a6b
HrishikShaji <hrishikminishaji@gmail.com>
2024-02-23 12:15:53 +0530

    addded ref to fetaure

--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -3,15 +3,14 @@ import { data } from "../lib/data";
 import { useRef } from "react";
 import gsap from "gsap";
 import { AnimateText } from "./AnimateText";
-import { Feature } from "./Feature";
 
 export const Features = () => {
 	const mainRef = useRef<HTMLDivElement>(null);
-
+	const featureRefs = useRef<(HTMLDivElement | null)[]>([]);
 	useGSAP(
 		() => {
 			if (mainRef.current) {
-				gsap.from(".feature", {
+				gsap.from(featureRefs.current, {
 					stagger: 0.3,
 					y: 300,
 					scrollTrigger: {
@@ -29,7 +28,14 @@ export const Features = () => {
 	return (
 		<div ref={mainRef} className="grid grid-cols-2  z-40 ">
 			{data.features.data.map((item, i) => (
-				<Feature item={item} key={i} />
+				<div
+					ref={(el) => (featureRefs.current[i] = el)}
+					key={i}
+					className=" gap-4 p-20 bg-[#fc7eff]  text-center w-full  flex flex-col"
+				>
+					<AnimateText word={item.title} position="-80% bottom" />
+					<h1 className="text-2xl font-inter">{item.desc}</h1>
+				</div>
 			))}
 		</div>
 	);
